73c814d9d101c0df4ae271f084a1445f4a5e8b6a
sheep <sheep@sheepdev.xyz>
2022-08-27 18:49:02 +0200

    ✨ Make it work outside of localhost + some other thingies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://willyjl.dev/smoothievid",
+  base: "/smoothievid",
   output: "server",
   adapter: node(),
   integrations: [tailwind()],
