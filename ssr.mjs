e3de537de4b61675ae1866fb843b47d34eaf313d
Willy-JL <willy.leslie@icloud.com>
2022-08-27 19:43:05 +0200

    Fix production base url

--- ssr.mjs
+++ ssr.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import { handler as ssrHandler } from './dist/server/entry.mjs';
 
 const app = express();
+app.set('base', '/smoothievid');
 
 app.use((_, res, next) => {
     res.header('Cross-Origin-Opener-Policy', 'same-origin');
