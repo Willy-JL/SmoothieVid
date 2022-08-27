11182d6d8d570dbf2103aad03701a358876d1155
Willy-JL <willy.leslie@icloud.com>
2022-08-27 21:52:35 +0200

    Fix main script

--- ssr.mjs
+++ ssr.mjs
@@ -2,16 +2,18 @@ import express from 'express';
 import { handler as ssrHandler } from './dist/server/entry.mjs';
 
 const app = express();
-app.set('base', '/smoothievid');
+const port = process.env.PORT || 3000
 
 app.use((_, res, next) => {
-    res.header('Cross-Origin-Opener-Policy', 'same-origin');
-    res.header('Cross-Origin-Resource-Policy', 'same-origin');
-    res.header('Cross-Origin-Embedder-Policy', 'require-corp');
-    next();
-  });
-  
+  res.header('Cross-Origin-Opener-Policy', 'same-origin');
+  res.header('Cross-Origin-Resource-Policy', 'same-origin');
+  res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
+
 app.use(express.static('dist/client/'));
 app.use(ssrHandler);
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}...`);
+});
