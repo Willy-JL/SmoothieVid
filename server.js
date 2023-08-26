de077ce3d5505313ea16a98843cb3f26ba3cb529
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2023-08-26 22:44:29 +0200

    Just in case

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.use((_, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Cross-Origin-Opener-Policy', 'same-origin');
     res.header('Cross-Origin-Resource-Policy', 'same-origin');
     res.header('Cross-Origin-Embedder-Policy', 'require-corp');
