000879140d7372a14b08277386a565e483c961c5
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-04-22 09:37:10 +0100

    Explain why CORS is required

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.use((_, res, next) => {
+    // Required config for SharedArrayBuffer, which is necessary for ffmpeg.wasm
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Cross-Origin-Opener-Policy', 'same-origin');
     res.header('Cross-Origin-Resource-Policy', 'same-origin');
